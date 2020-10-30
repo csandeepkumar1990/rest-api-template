bd3a3b3b0feff1eff9d5485c274dec25e9a720db
csandeepkumardev01 <64784427+csandeepkumardev01@users.noreply.github.com>
2020-10-30 12:11:17 +0530

    pushed angular

---
+++ models/supportdashboard.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Supportdashboard extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Supportdashboard.init({
+    name: DataTypes.STRING,
+    type: DataTypes.STRING,
+    description: DataTypes.STRING,
+    name: DataTypes.STRING,
+    type: DataTypes.STRING,
+    description: DataTypes.STRING,
+    createdBy: DataTypes.STRING,
+    updatedBy: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Supportdashboard',
+  });
+  return Supportdashboard;
+};
